ab13d1f1f4a048ad4ca1c351365c87acec41da9f
RaghavKhurana2104 <raghavkhurana60@gmail.com>
2022-08-11 22:30:16 +0530

    touch up

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for(i=p; i<p+50; i++){
     content1 += `
     <div style="text-align;">
         <a href="${imag}"><img src="${id}" style="margin-right: 30px; margin-left:30px;"></a>
-        <div style="width:150px; margin-left:50px; text-align: center;color:white;">
+        <div style="width:150px; margin-left:30px; text-align: center;color:white;">
         <p style="overflow:auto">${title}</p>
         </div>
     </div>`
