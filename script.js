971d4904635810e7ef72ade47ad78e7c91339a86
RaghavKhurana2104 <raghavkhurana60@gmail.com>
2022-08-11 18:33:04 +0530

    adding elements

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ xhr.onload= function(){
 let content1 ="";
 let p=0;
 const images=(i)=>{
-for(i=p; i<p+50; i++){
+for(i=p; i<p+48; i++){
     let id = response[i].thumbnailUrl;
     let title = response[i].title;
     content1 += `
     <div style="text-align;">
-        <img src="${id}" style="margin-right: 50px; margin-left:50px;">
+        <img src="${id}" style="margin-right: 30px; margin-left:30px;">
         <div style="width:150px; margin-left:50px; text-align: center;">
         <p style="overflow:auto">${title}</p>
         </div>
@@ -29,7 +29,7 @@ images(p);
 
 document.getElementById('more').addEventListener('click',()=>{
     console.log(p);
-    p=p+50;
+    p=p+48;
     if(p<5000){
         images(p);
     }
